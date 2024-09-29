1ae12b55393e848f18d947a6181f43453a94d231
Prathamesh Shinde <sprathamesh354@gmail.com>
2024-09-29 20:18:04 +0530

    Guardial's repository has been created!

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,12 +1,29 @@
+import { useState, useEffect} from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import Signup from './pages/Signup';
 
-
+import { AuthProvider } from './context/authContext'
+import Login from './pages/Login';
 function App() {
 
   return (
-    <>
-      <h1 className="p-12 bg-[#20C997] ">Welcome To Guradial!</h1>
-    </>
+    
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+    </Router>
   )
 }
 
-export default App
+function AppWrapper(){
+  return (
+    <AuthProvider>
+        <App />
+    </AuthProvider>
+    
+  )
+}
+export default AppWrapper
+
---
+++ FRONTEND/src/context/authContext.jsx
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useEffect } from 'react';
+import axios from '../config/axiosConfig'; 
+import { toast } from 'react-toastify';
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await axios.get('/check-auth');
+        setIsLoggedIn(response.data.isAuthenticated);
+        setCurrentUser(response.data.user); 
+      } catch (error) {
+        setIsLoggedIn(false);
+        setCurrentUser(null);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  const login = (userData) => {
+    setCurrentUser(userData);
+    setIsLoggedIn(true);
+  };
+
+  const logout = async () => {
+    try {
+      await axios.post('/logout');
+    } catch (error) {
+      toast.error('Logout failed!');
+    }
+    setCurrentUser(null);
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, currentUser, login, logout, setCurrentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
